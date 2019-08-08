8b5a97ef9b181294258a924a43856cfa597065b7
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-08-08 02:28:12 -0400

    replaced function component with class components

--- webtesting-ii/src/App.js
+++ webtesting-ii/src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default App;
